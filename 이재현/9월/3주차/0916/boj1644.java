63e5cea53faba899e29dceca1b042b01566aeaf3
fanta4715 <fanta4715@naver.com>
2023-09-16 16:46:22 +0900

    똥치우기2

--- 이재현/9월/3주차/0916/boj1644.java
+++ 이재현/9월/3주차/0916/boj1644.java
@@ -1,3 +1,4 @@
+package a230916;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -5,7 +6,7 @@ import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.StringTokenizer;
 
-public class Main {
+public class boj1644 {
     public static void main(String[] args) throws IOException {
         BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
 
