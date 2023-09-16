37433679641daf4ac5e2e4f4eaa4164dc921b264
fanta4715 <fanta4715@naver.com>
2023-09-16 16:41:33 +0900

    똥 치우기

--- out/production/2023_Sudal_Algorithm/김강민/9월/Week_03/0914/12969.py
+++
@@ -1,7 +0,0 @@
-import sys
-input = sys.stdin.readline
-
-n, m = map(int, input().split())
-
-for i in range(m):
-    print("*" * n)
--- out/production/2023_Sudal_Algorithm/김강민/9월/Week_03/0915/12933.py
+++
@@ -1,4 +0,0 @@
-def solution(n):
-    n = list(str(n))
-    n.sort(reverse=True)
-    return int("".join(n))
