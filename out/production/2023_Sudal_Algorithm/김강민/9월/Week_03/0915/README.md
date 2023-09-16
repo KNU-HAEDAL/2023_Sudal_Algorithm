deb2e4c04aa19708de01a1fc9aea5bd3ba458b26
fanta4715 <fanta4715@naver.com>
2023-09-16 16:13:32 +0900

    gold : two pointer

---
+++ out/production/2023_Sudal_Algorithm/김강민/9월/Week_03/0915/README.md
@@ -0,0 +1,47 @@
+# [level 1] 정수 내림차순으로 배치하기 - 12933
+
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/12933)
+
+### 성능 요약
+
+메모리: 10.3 MB, 시간: 0.02 ms
+
+### 구분
+
+코딩테스트 연습 > 연습문제
+
+### 채점결과
+
+Empty
+
+### 문제 설명
+
+<p>함수 solution은 정수 n을 매개변수로 입력받습니다. n의 각 자릿수를 큰것부터 작은 순으로 정렬한 새로운 정수를 리턴해주세요. 예를들어 n이 118372면 873211을 리턴하면 됩니다.</p>
+
+<h5>제한 조건</h5>
+
+<ul>
+<li><code>n</code>은 1이상 8000000000 이하인 자연수입니다.</li>
+</ul>
+
+<h5>입출력 예</h5>
+<table class="table">
+        <thead><tr>
+<th>n</th>
+<th style="text-align: center">return</th>
+</tr>
+</thead>
+        <tbody><tr>
+<td>118372</td>
+<td style="text-align: center">873211</td>
+</tr>
+</tbody>
+      </table>
+
+> 출처: 프로그래머스 코딩 테스트 연습, https://programmers.co.kr/learn/challenges
+
+## 풀이
+
+1. 정수로 입력받은 n을 문자열로 바꾸고, list의 형태로 바꾼다.
+2. n을 반대로 정렬 한다.
+3. 다시 n을 int로 만든 후 출력한다.
---
+++ 이재현/9월/3주차/0916/boj1644.java
@@ -0,0 +1,62 @@
+package a230916;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
+public class boj1644 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =new BufferedReader(new InputStreamReader(System.in));
+
+        //N을 받음
+        int N = Integer.parseInt(br.readLine());
+
+        //~~의 체로 소수 걸러서 ArrayList에 넣어둠
+        boolean [] prime = new boolean[N+1];
+        for (int i=0;i<N+1;i++) prime[i]=true;
+
+        prime[0]=false;
+        prime[1]=false;
+        ArrayList<Integer> primeList=new ArrayList<>();
+
+        for (int i=2; i*i<=N; i++){
+            //소수가 아니면, skip
+            if (!prime[i]) continue;
+
+            //해당 값이 소수이면, 배수를 전부 제외시킴
+            // arraylist에 넣기
+//            primeList.add(i);
+            for (int j=i*i; j<=N; j=j+i){
+                prime[j]=false;
+            }
+
+        }
+
+        for (int i=2;i<N+1;i++){
+            if (prime[i]) primeList.add(i);
+        }
+
+        //넣어둔 소수 리스트 바탕으로 투 포인터
+        int left=0,right=0;
+        int size = primeList.size();
+        int sum=0;
+        int answer=0;
+
+        // 2 3 5 7 11 13 17 19 23 ...
+        while (left<size){
+            while(right<size && sum+primeList.get(right)<=N ){
+                sum+=primeList.get(right);
+                right++;
+            }
+            if (sum==N) answer++;
+            if (right>=size)break;
+            sum-=primeList.get(left);
+            left++;
+        }
+
+        //결과 반환
+        System.out.println(answer);
+    }
+}
