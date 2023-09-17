3d84df8b64a59bbbfaf1c531617dcea9f6d4bf59
soParkjeong <sojung0734@naver.com>
2023-09-17 15:50:52 +0900

    9월 16일 두 수의 나눗셈

---
+++ 박소정/9월/3주차/0916/120806.java
@@ -0,0 +1,7 @@
+class Solution {
+    public int solution(int num1, int num2) {
+       double answer=((double)num1/num2);
+        int finalAnswer=(int)(answer*1000);
+        return finalAnswer;
+    }
+}
