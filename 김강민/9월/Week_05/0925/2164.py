c663022dde535d7ecbeebd5b233151e503abf2bb
Dobbymin <gmin0701@knu.ac.kr>
2023-09-26 02:21:40 +0900

    [FEAT] 9.25 카드2

---
+++ 김강민/9월/Week_05/0925/2164.py
@@ -0,0 +1,14 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+deque = deque([i for i in range(1, N + 1)])
+
+while(len(deque) > 1):
+    deque.popleft()
+    move_num = deque.popleft()
+    deque.append(move_num)
+
+print(deque[0])
