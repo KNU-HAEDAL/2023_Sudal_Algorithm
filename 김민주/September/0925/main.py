0dcc701a5c018cb50a3d1117c87df68f2ae9c9c4
manjookim <106953846+manjookim@users.noreply.github.com>
2023-09-25 15:17:31 +0900

    할인해줘

---
+++ 김민주/September/0925/main.py
@@ -0,0 +1,9 @@
+def solution(price):
+    answer = price
+    if price >= 500000:
+        answer = price*0.8
+    elif price >= 300000:
+        answer = price*0.9
+    elif price >= 100000:
+        answer = price*0.95
+    return int(answer)
