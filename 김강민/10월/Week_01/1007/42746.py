85d2f8806eff7db6d733f00c37f19143c239a0ed
Dobbymin <gmin0701@knu.ac.kr>
2023-10-07 09:52:10 +0900

    ✨feat: 10.7 가장 큰 수

---
+++ 김강민/10월/Week_01/1007/42746.py
@@ -0,0 +1,6 @@
+def solution(numbers):
+    num = [str(i) for i in numbers]
+    num = sorted(num, key = lambda x: x*3, reverse = True)
+    return str(int(''.join(num)))
+
+print(solution([6, 10, 2]))
