14624c01a51a8a1e435c938d71e5face6c5c16f1
Dobbymin <gmin0701@knu.ac.kr>
2023-11-03 07:32:06 +0900

    ✨ feat: 11.02 K번째 수

---
+++ 김강민/11월/1주차/1102/42748.js
@@ -0,0 +1,22 @@
+function solution(array, commands) {
+    let result = [];
+    let i = 0;
+    let j = 0;
+    let k = 0;
+
+    for (let n = 0; n < commands.length; n++) {
+        i = commands[n][0];
+        j = commands[n][1];
+        k = commands[n][2];
+
+        let sliced = array.slice(i - 1, j);
+        let sorted = sliced.sort((a, b) => a - b);
+
+        result.push(sorted[k - 1]);
+    }
+
+    return result;
+}
+
+// i번째 ~ j번째까지 자른 후 정렬
+// 정렬된 배열에서 k번째의 수 구하기
