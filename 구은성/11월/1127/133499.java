fddceb74ec75aa05b33789ffc963065e684b2e82
Koo-EunSung <ku903@naver.com>
2023-11-27 22:31:21 +0900

    Feat: 11월 27일 옹알이(2)

---
+++ 구은성/11월/1127/133499.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int solution(String[] babbling) {
+        int answer = 0;
+        String[] avail = {"aya", "ye", "woo", "ma"};
+        
+        for(String s : babbling) {
+            for(String availPro : avail) {
+                if(s.contains(availPro)) {
+                    if(!s.contains(availPro + availPro))
+                        s = s.replace(availPro, "-");
+                }
+            }
+            
+            s = s.replaceAll("-" , "");
+            if(s.equals("")) {
+                answer++;
+            }
+        }
+        
+        return answer;
+    }
+}
