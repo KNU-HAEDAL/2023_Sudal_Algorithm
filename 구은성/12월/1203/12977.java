30e8f1dbbf22be6de4870fd647ae1db9e5a223c3
Koo-EunSung <ku903@naver.com>
2023-12-03 22:53:58 +0900

    Feat: 12월 3일 소수 만들기

---
+++ 구은성/12월/1203/12977.java
@@ -0,0 +1,27 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+
+        for(int i = 0; i < nums.length - 2; i++) {
+            for(int j = i + 1; j < nums.length - 1; j++) {
+                for(int k = j + 1; k < nums.length; k++) {
+                    int sum = nums[i] + nums[j] + nums[k];
+                    if(isPrime(sum)) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public boolean isPrime(int n) {
+        for(int i = 2; i <= Math.sqrt(n); i++) {
+            if(n % i == 0) {
+                return false;
+            }
+        }
+        return true;
+    }
+}
