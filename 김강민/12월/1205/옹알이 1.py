9b97e9efb9c4dcd9038ab5be1c417e2887e0e22c
Dobbymin <gmin0701@knu.ac.kr>
2023-12-06 23:16:36 +0900

    ✨ feat: 12.05 옹아리 (1) 문제 풀이 코드 작성"

---
+++ 김강민/12월/1205/옹알이 1.py
@@ -0,0 +1,16 @@
+def solution(babbling):
+    cnt = 0
+    
+    for babble in babbling:
+        while babble:
+            if babble[:3] in ['aya', 'woo']:
+                babble = babble[3:]
+            elif babble[:2] in ['ye', 'ma']:
+                babble = babble[2:]
+            else:
+                break
+        if babble == '':
+            cnt += 1
+                
+            
+    return cnt
