b1589a6f103cbb51373da4cbff7a20d5a7d50a06
Koo-EunSung <ku903@naver.com>
2023-12-15 23:15:56 +0900

    Feat: 12월 15일 둘만의 암호

---
+++ 구은성/12월/1215/155652.java
@@ -0,0 +1,23 @@
+class Solution {
+    public String solution(String s, String skip, int index) {
+        String answer = "";
+        
+        for(int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            for(int j = 0; j < index; j++) {
+                ch += 1;
+                if(ch > 'z') {
+                    ch -= 26;
+                }
+                
+                if(skip.contains(Character.toString(ch))) {
+                    j--;
+                }
+            }
+            
+            answer += ch;
+        }
+        
+        return answer;
+    }
+}
